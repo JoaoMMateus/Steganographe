ccf2b64aaeafd6136a187ddf413198c598a8ef7a
Unknown <joao.martinsmateus@epsi.fr>
2018-04-06 17:50:23 +0200

    Ajout des ressources utilisé

--- app/upload.php
+++ app/upload.php
@@ -1,71 +1,71 @@
 <?php
 
-    // if (!empty($_FILES["SoundFile"])) {
-    //     $myFile = $_FILES["SoundFile"];
+    if (!empty($_FILES["SoundFile"])) {
+        $myFile = $_FILES["SoundFile"];
     
-    //     if ($myFile["error"] !== UPLOAD_ERR_OK) {
-    //         echo "<p>An error occurred.</p>";
-    //         exit;
-    //     }
+        if ($myFile["error"] !== UPLOAD_ERR_OK) {
+            echo "<p>An error occurred.</p>";
+            exit;
+        }
     
-    //     // ensure a safe filename
-    //     $name = preg_replace("/[^A-Z0-9._-]/i", "_", $myFile["name"]);
+        // ensure a safe filename
+        $name = preg_replace("/[^A-Z0-9._-]/i", "_", $myFile["name"]);
     
-    //     // don't overwrite an existing file
-    //     $i = 0;
-    //     $parts = pathinfo($name);
-    //     while (file_exists("uploads/son/" . $name)) {
-    //         $i++;
-    //         $name = $parts["filename"] . "-" . $i . "." . $parts["extension"];
-    //     }
+        // don't overwrite an existing file
+        $i = 0;
+        $parts = pathinfo($name);
+        while (file_exists("uploads/son/" . $name)) {
+            $i++;
+            $name = $parts["filename"] . "-" . $i . "." . $parts["extension"];
+        }
     
-    //     // preserve file from temporary directory
-    //     $success = move_uploaded_file($myFile["tmp_name"],"uploads/son/" . $name);
-    //     if (!$success) { 
-    //         echo "<p>Unable to save file.</p>";
-    //         exit;
-    //     }
+        // preserve file from temporary directory
+        $success = move_uploaded_file($myFile["tmp_name"],"uploads/son/" . $name);
+        if (!$success) { 
+            echo "<p>Unable to save file.</p>";
+            exit;
+        }
     
-    //     // set proper permissions on the new file
-    //     // chmod("uploads/son/" . $name, 0644);
-    // }
-    // if (!empty($_FILES["PictureFile"])) {
-    //     $myFile = $_FILES["PictureFile"];
+        // set proper permissions on the new file
+        // chmod("uploads/son/" . $name, 0644);
+    }
+    if (!empty($_FILES["PictureFile"])) {
+        $myFile = $_FILES["PictureFile"];
     
-    //     if ($myFile["error"] !== UPLOAD_ERR_OK) {
-    //         echo "<p>An error occurred.</p>";
-    //         exit;
-    //     }
+        if ($myFile["error"] !== UPLOAD_ERR_OK) {
+            echo "<p>An error occurred.</p>";
+            exit;
+        }
     
-    //     // ensure a safe filename
-    //     $name = preg_replace("/[^A-Z0-9._-]/i", "_", $myFile["name"]);
+        // ensure a safe filename
+        $name = preg_replace("/[^A-Z0-9._-]/i", "_", $myFile["name"]);
     
-    //     // don't overwrite an existing file
-    //     $i = 0;
-    //     $parts = pathinfo($name);
-    //     while (file_exists("uploads/images/" . $name)) {
-    //         $i++;
-    //         $name = $parts["filename"] . "-" . $i . "." . $parts["extension"];
-    //     }
+        // don't overwrite an existing file
+        $i = 0;
+        $parts = pathinfo($name);
+        while (file_exists("uploads/images/" . $name)) {
+            $i++;
+            $name = $parts["filename"] . "-" . $i . "." . $parts["extension"];
+        }
     
-    //     // preserve file from temporary directory
-    //     $success = move_uploaded_file($myFile["tmp_name"],"uploads/images/" . $name);
-    //     if (!$success) { 
-    //         echo "<p>Unable to save file.</p>";
-    //         exit;
-    //     }
+        // preserve file from temporary directory
+        $success = move_uploaded_file($myFile["tmp_name"],"uploads/images/" . $name);
+        if (!$success) { 
+            echo "<p>Unable to save file.</p>";
+            exit;
+        }
     
-    //     // set proper permissions on the new file
-    //     // chmod("uploads/image/" . $name, 0644);
-    // }
+        // set proper permissions on the new file
+        // chmod("uploads/image/" . $name, 0644);
+    }
 
     //methode permetant l'execution d'un script python avec des arguments/parametres
-    // $myfile = fopen("uploads/messages/newfile.txt", "w") or die("Unable to open file!");
-    // $txt = "John Doe\n";
-    // fwrite($myfile, $txt);
-    // $txt = "Jane Doe\n";
-    // fwrite($myfile, $txt);
-    // fclose($myfile);
+    $myfile = fopen("uploads/messages/newfile.txt", "w") or die("Unable to open file!");
+    $txt = "John Doe\n";
+    fwrite($myfile, $txt);
+    $txt = "Jane Doe\n";
+    fwrite($myfile, $txt);
+    fclose($myfile);
     $soundfile = "uploads/son/bird.wav";
     $soundfileover40k = "uploads/son/bubbling1.wav";
     $osoundfile = "uploads/son/newfile2.wav";
