d13670dd335ae6eb57ccb4aa078cb10daf19ae1e
Unknown <joao.martinsmateus@epsi.fr>
2018-04-08 16:10:05 +0200

    mise a jour gitignore et fichier de teste

--- app/test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3.6
-import sys
-
-print("Hello World!")
-print("Number of arguments:", len(sys.argv), "arguments.")
-print("Argument List:", str(sys.argv))
-print("Argument n°2:", str(sys.argv[2]))
